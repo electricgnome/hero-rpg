1c8aeb879e2985ea462afd1104537737839817fe
Hector <hector@electricgnome.com>
2018-03-15 09:23:18 -0500

    added more features to RPG

--- character_lib.py
+++ character_lib.py
@@ -1,13 +1,14 @@
 from random import randint as randint
 
 class Character(object):
-    def __init__(self, race, health, power, coins, items=[], attacked=False):
+    def __init__(self, race, health, power, coins, items=[], attacked=False, armor=0):
         self.race=race
         self.health=health
         self.power=power
         self.coins=coins
         self.items=items
         self.attacked=attacked
+        self.armor=armor
 
     def __str__(self):
         return 'Character: {} has {} health, {} power and {} coins'.format(self.race, self.health, self.power, self.coins)
@@ -21,19 +22,28 @@ class Character(object):
         else:
             return False
 
-    def special(self):
+    def special(self, monster):
         if self.attacked:
+            defense= monster.power - self.armor
+            print("OW!")
+            self.health -= defense
             self.attacked = False
 
+    def buy(self, item):
+        self.coins -= item.cost
+        item.apply(hero)
 
     def attack(self, monster):
         # monster.health, self.health -=self.power, monster.power
-        monster.health -=self.power
-        if monster.alive:
+        if monster.alive():
+            monster.health -=self.power
             monster.attacked = True
-        print("The {} bashed the {} for {} damage".format(self.race, monster.race, self.power))
-        print("The {} hit the {} back for {} damage".format(monster.race, self.race, monster.power))
-        monster.special()
+            print("The {} bashed the {} for {} damage".format(self.race, monster.race, self.power))
+            print("The {} hit the {} back for {} damage".format(monster.race, self.race, monster.power))
+            monster.special(monster)
+        else:
+            self.coins +=monster.coins
+            print(monster.race, "is dead, you can move on now!")
 
 
 
@@ -43,24 +53,30 @@ class Hero(Character):
         super().__init__(race, health, power, coins, attacked=False)
 
     def attack(self, monster):
-        if randint(0,100) <20:
-            pwr=self.power *2
-        else:
-            pwr=self.power
-        # monster.health, self.health -=pwr, monster.power
-        monster.health -=pwr
-        if monster.alive:
+        if monster.alive():
+            if randint(0,100) <20:
+                pwr=self.power *2
+            else:
+                pwr=self.power
+            # monster.health, self.health -=pwr, monster.power
+            monster.health -=pwr
             monster.attacked = True
-        print("The {} bashed the {} for {} damage".format(self.race, monster.race, pwr))
-        print("The {} hit the {} back for {} damage".format(monster.race, self.race, monster.power))
-        monster.special()
+            self.attacked=True
+            print("The {} bashed the {} for {} damage".format(self.race, monster.race, pwr))
+            print("The {} hit the {} back for {} damage".format(monster.race, self.race, monster.power))
+            monster.special(self)
+            self.special(monster)
+        else:
+            self.coins +=monster.coins
+            print(monster.race, " is dead, you can move on now!")
+
 
 
 class Medic(Character):
     def __init__(self, race, health, power, coins, attacked=False):
         super().__init__(race, health, power, coins, attacked=False)
 
-    def special(self):
+    def special(self, monster):
         if self.attacked:
             if randint(0,100) <20:
                 self.health =self.health +2
@@ -91,6 +107,61 @@ class Wizard(Character):
         super().__init__(race, health, power, coins, attacked=False)
 
     def attack(self, monster):
+        swap =randint(0,100) < 50
+        if swap:
+            print ("{} swaps power with {} during attack!".format(self.race, monster.race))
+            self.power, monster.power = monster.power, self.power
+        super(Wizard, self).attack(monster)
+        if swap:
+            self.power, monster.power = monster.power, self.power
+
+
+class Tonic(object):
+    cost = 5
+    name = 'Tonic'
+
+    def apply(self, character):
+        character.health +=5
+        print("{}'s health increased to {}".format(character.race, character.health))
+
+class Sword(object):
+    cost = 10
+    name='Sword'
+
+    def apply(self, character):
+        character.power +=2
+        print("{}'s power increased to {}".format(character.race, character.power))
+
+class Armor(object):
+    cost=15
+    name='Armor'
+
+    def apply(self, character):
+        character.armor= 2
+
+class Store(object):
+    items=[Armor, Sword, Tonic]
+
+    def shop(character):
+        while True:
+            print("=====================")
+            print("Welcome to the store!")
+            print("=====================")
+            print("You have {} coins.".format(character.coins))
+            print("What do you want to do?")
+            for i in range(len(Store.items)):
+                item = Store.items[i]
+                print("{}. buy {} ({})".format(i + 1, item.name, item.cost))
+            print("10. leave")
+            choice = int(input("> "))
+            if choice == 10:
+                break
+            elif character.coins >= Store.items[choice - 1].cost:
+                ItemToBuy = Store.items[choice - 1]
+                item = ItemToBuy()
+                character.buy(item)
+            else:
+                print("You can't afford that!")
 
 
 goblin=Character('Goblin', 50, 2, 5)
