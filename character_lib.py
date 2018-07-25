b50ca2532020b26a14847b7369f3e70f18a9def2
Hector <hector@electricgnome.com>
2018-07-25 14:12:29 -0500

    finalize

--- character_lib.py
+++ character_lib.py
@@ -2,20 +2,20 @@ from random import randint as randint
 
 class Character(object):
     def __init__(self, race, health, power, coins, items=[], attacked=False, armor=0):
-        self.race=race
-        self.health=health
-        self.power=power
-        self.coins=coins
-        self.items=items
-        self.attacked=attacked
-        self.armor=armor
+        self.race = race
+        self.health = health
+        self.power = power
+        self.coins = coins
+        self.items = items
+        self.attacked = attacked
+        self.armor = armor
 
     def __str__(self):
-        return 'Character: {} has {} health, {} power and {} coins'.format(self.race, self.health, self.power, self.coins)
-
+        return ('Character: {} has {} health, {} power and {} coins'.format(self.race, self.health, self.power, self.coins))
+        
     def status(self):
-        return 'Character: {} has {} health, {} power and {} coins'.format(self.race, self.health, self.power, self.coins)
-
+        print ('Character: {} has {} health, {} power and {} coins'.format(self.race, self.health, self.power, self.coins))
+        return
     def alive(self):
         if self.health > 0:
             return True
@@ -24,7 +24,7 @@ class Character(object):
 
     def special(self, monster):
         if self.attacked:
-            defense= monster.power - self.armor
+            defense = monster.power - self.armor
             print("OW!")
             self.health -= defense
             self.attacked = False
@@ -34,18 +34,18 @@ class Character(object):
         item.apply(hero)
 
     def attack(self, monster):
-        # monster.health, self.health -=self.power, monster.power
+       
         if monster.alive():
-            monster.health -=self.power
+            monster.health -= self.power
             monster.attacked = True
-            print("The {} bashed the {} for {} damage".format(self.race, monster.race, self.power))
-            print("The {} hit the {} back for {} damage".format(monster.race, self.race, monster.power))
+            print("The {} bashed the {} for {} damage".format(
+                self.race, monster.race, self.power))
+            print("The {} hit the {} back for {} damage".format(
+                monster.race, self.race, monster.power))
             monster.special(monster)
         else:
-            self.coins +=monster.coins
+            self.coins += monster.coins
             print(monster.race, "is dead, you can move on now!")
-
-
 
 
 class Hero(Character):
@@ -54,22 +54,27 @@ class Hero(Character):
 
     def attack(self, monster):
         if monster.alive():
-            if randint(0,100) <20:
-                pwr=self.power *2
+            if randint(0, 100) < 20:
+                pwr = self.power * 2
             else:
-                pwr=self.power
-            # monster.health, self.health -=pwr, monster.power
-            monster.health -=pwr
+                pwr = self.power
+           
+            monster.health -= pwr
             monster.attacked = True
-            self.attacked=True
-            print("The {} bashed the {} for {} damage".format(self.race, monster.race, pwr))
-            print("The {} hit the {} back for {} damage".format(monster.race, self.race, monster.power))
+            self.attacked = True
+            print("The {} bashed the {} for {} damage".format(
+                self.race, monster.race, pwr))
+            print("The {} hit the {} back for {} damage".format(
+                monster.race, self.race, monster.power))
             monster.special(self)
             self.special(monster)
-        else:
-            self.coins +=monster.coins
-            print(monster.race, " is dead, you can move on now!")
+            if not monster.alive():
+                self.coins += monster.coins
+                print(monster.race, " is dead, you can move on now!")
 
+        else:
+            # self.coins += monster.coins
+            print(monster.race, " is dead, you can move on now!")
 
 
 class Medic(Character):
@@ -78,25 +83,19 @@ class Medic(Character):
 
     def special(self, monster):
         if self.attacked:
-            if randint(0,100) <20:
-                self.health =self.health +2
+            if randint(0, 100) < 20:
+                self.health = self.health + 2
                 print("The medic healed!")
             self.attacked = False
 
-class Zombie(Character):
-    def __init__(self, race, health, power, coins, attacked=False):
-        super().__init__(race, health, power, coins, attacked=False)
-
-    def alive(self):
-        return True
 
 class Shadow(Character):
     def __init__(self, race, health, power, coins, attacked=False):
         super().__init__(race, health, power, coins, attacked=False)
 
-    def special(self):
+    def special(self, monster):
         if self.attacked:
-            if randint(0,100) <90:
+            if randint(0, 100) < 90:
                 self.health = 1
                 print("You missed!!")
             self.attacked = False
@@ -107,44 +106,58 @@ class Wizard(Character):
         super().__init__(race, health, power, coins, attacked=False)
 
     def attack(self, monster):
-        swap =randint(0,100) < 50
+        swap = randint(0, 100) < 50
         if swap:
-            print ("{} swaps power with {} during attack!".format(self.race, monster.race))
+            print("{} swaps power with {} during attack!".format(
+                self.race, monster.race))
             self.power, monster.power = monster.power, self.power
         super(Wizard, self).attack(monster)
         if swap:
             self.power, monster.power = monster.power, self.power
 
+class Zombie(Character):
+    def __init__(self, race, health, power, coins, attacked=False):
+        super().__init__(race, health, power, coins, attacked=False)
+
+    def alive(self):
+        zombie.health = randint(1,5)
+        return True
 
 class Tonic(object):
     cost = 5
     name = 'Tonic'
 
     def apply(self, character):
-        character.health +=5
-        print("{}'s health increased to {}".format(character.race, character.health))
+        character.health += 5
+        print("{}'s health increased to {}".format(
+            character.race, character.health))
+
 
 class Sword(object):
     cost = 10
-    name='Sword'
+    name = 'Sword'
 
     def apply(self, character):
-        character.power +=2
-        print("{}'s power increased to {}".format(character.race, character.power))
+        character.power += 2
+        print("{}'s power increased to {}".format(
+            character.race, character.power))
+
 
 class Armor(object):
-    cost=15
-    name='Armor'
+    cost = 15
+    name = 'Armor'
 
     def apply(self, character):
-        character.armor= 2
+        character.armor = 2
+
 
 class Store(object):
-    items=[Armor, Sword, Tonic]
+    
+    items = [Armor, Sword, Tonic]
 
-    def shop(character):
+    def shop(self, character):
         while True:
-            print("=======*==============")
+            print("=====================")
             print("Welcome to the store!")
             print("=====================")
             print("You have {} coins.".format(character.coins))
@@ -152,33 +165,51 @@ class Store(object):
             for i in range(len(Store.items)):
                 item = Store.items[i]
                 print("{}. buy {} ({})".format(i + 1, item.name, item.cost))
-            print("10. leave")
+            print("0. leave")
             choice = int(input("> "))
-            if choice == 10:
-                break
-            elif character.coins >= Store.items[choice - 1].cost:
-                ItemToBuy = Store.items[choice - 1]
-                item = ItemToBuy()
-                character.buy(item)
+            try:
+                Store.items[choice-1]
+            except IndexError:
+                print("Invalid input, please choose between 1 and 3 or 0")
+                continue
             else:
-                print("You can't afford that!")
+                if choice == 0:
+                    break
+                elif character.coins >= Store.items[choice - 1].cost:
+                    ItemToBuy = Store.items[choice - 1]
+                    item = ItemToBuy()
+                    character.buy(item)
+                elif character.coins < Store.items[choice - 1].cost:
+                    print("You can't afford that!")
+
+                else:
+                    print("Invalid input, please choose between 1 and 3 or 0")
 
 
-goblin=Character('Goblin', 50, 2, 5)
-hero=Hero('Hero', 100, 5, 20)
-medic=Medic('Medic', 65,3,8)
-shadow=Shadow('Shadow',1,2,10)
-wizard=Wizard('Wizard', 8,1,6)
-zombie=Zombie('Zombie', 5, 1, 200)
-
-print(goblin)
-print(hero)
-print(medic)
-print(shadow)
-print(wizard)
-print(zombie)
+goblin = Character('Goblin', 50, 2, 5)
+hero = Hero('Hero', 100, 5, 20)
+medic = Medic('Medic', 65, 3, 8)
+shadow = Shadow('Shadow', 1, 2, 10)
+wizard = Wizard('Wizard', 8, 1, 6)
+zombie = Zombie('Zombie', 5, 1, 200)
+store = Store()
 
 
+# print(goblin)
+# print(hero)
+# print(medic)
+# print(shadow)
+# print(wizard)
+# print(zombie)
+# enemies = [goblin, medic, shadow, wizard, zombie]
+
+# for i in range(len(enemies)):
+#     print(i+1, enemies[i])
+#     print("> ", end=' ')
+
+# hero.attack(enemies[2])
+
+# hero.status()
 # hero.attack(goblin)
 # goblin.attack(hero)
 # while goblin.health > 0 and hero.health > 0:
--- main_rpg.py
+++ main_rpg.py
@@ -1,38 +1,76 @@
-enemies= ['goblin', 'medic', 'shadow', 'wizard', 'zombie']
+from character_lib import *
+
+
+enemies = [goblin, medic, shadow, wizard, zombie]
+
+
 def choose_enemy():
-    print ('Choose an enemy to fight!')
+    
+    print('Choose an enemy to fight!')
     for i in range(len(enemies)):
-        print (i+1, enemies[i])
-    print("> ", end=' ')
-    keyinput = int(input())
-    if keyinput == 1:
-        hero.attack(enemy)
-    elif keyinput == 2:
-        pass
-    elif keyinput == 3:
-        print("Goodbye.")
-        main_menu()
-    else:
-        print("Invalid input {}".format(input))
-        continue
+        print(i+1, enemies[i].race)
+    while(True):   
+        try:
+            enemy = enemies[(int(input("> "))-1)]
+        except IndexError:
+            print("Invalid input, please choose between 1 and 5")
+            continue
+        else:
+            if enemy in enemies:
+                hero.attack(enemy)
+                battle(enemy)
+            else:
+                print("Invalid input {}".format(input))
 
-def main_manu():
-    print("""What do you want to do?
-        1. Do battle
-        2. Go shopping
-        3. View my stats
-        9. Exit game""")
+            
+def battle(enemy):
+    while hero.alive() and enemy.alive():
+        print('Hero: {} {}: {}'.format(hero.health, enemy.race ,enemy.health))
+        print("""What now?
+            1. Strike again!
+            2. Do nothing
+            3. Flee!""")
+        print("> ", end=' ')
+        keyinput = int(input())
+        if keyinput == 1:
+            hero.attack(enemy)
+            
+        elif keyinput == 2:
+            pass
+        elif keyinput == 3:
+            print("coward!")
+            main_menu()
+        else:
+            print("Invalid input. Please choose between 1 and 3")
+    main_menu()
 
-    keyinput = int(input())
-    if keyinput == 1:
-        choose_enemy()
-    elif keyinput == 2:
-        Store.shop(hero)
-    elif keyinput == 3:
-        hero.status()
-    elif keyinput == 9:
-        print("Goodbye.")
-        exit(0)
-    else:
-        print("Invalid input {}".format(input))
-        continue
+def main_menu():
+    while(True):
+        print("""What do you want to do?
+            1. Do battle
+            2. Go shopping
+            3. View my stats
+            0. Exit game""")
+        try:
+            keyinput = int(input())
+        except ValueError:
+            print("Invalid input, please choose between 1 and 3 or 0")
+            continue
+        else:
+            if keyinput == 1:
+                choose_enemy()
+            elif keyinput == 2:
+                store.shop(hero)
+                main_menu()
+
+            elif keyinput == 3:
+                hero.status()
+                main_menu()
+            elif keyinput == 0:
+                print("Goodbye.")
+                exit(0)
+            else:
+                print("Invalid input, please choose between 1 and 3 or 0")
+
+
+main_menu()
